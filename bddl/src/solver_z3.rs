cd7854358ca47ce49776f78628351d2d281de962
Daniel Anker Hermansen <daniel201001@gmail.com>
2023-10-29 21:30:34 +0100

    symbolic board

--- bddl/src/solver_z3.rs
+++ bddl/src/solver_z3.rs
@@ -2,7 +2,7 @@ use std::iter::repeat;
 
 use z3::{Context, ast::{BV, Bool, Ast, Dynamic, forall_const, Datatype, exists_const}, FuncDecl, Sort, DatatypeBuilder, DatatypeSort};
 
-use crate::bddl::{InitPred, Pred, SubCondition, Size, Condition, Problem, Domain, E};
+use crate::bddl::{InitPred, Pred, SubCondition, Size, Condition, Problem, Domain, E, Action};
 
 fn gen_bounds_check<'ctx>(e: &E, v: &BV<'ctx>, max: i64) -> Bool<'ctx> {
     match e {
@@ -12,12 +12,30 @@ fn gen_bounds_check<'ctx>(e: &E, v: &BV<'ctx>, max: i64) -> Bool<'ctx> {
     }
 }
 
+fn e_to_bv<'ctx>(e: &E, x: &BV<'ctx>, sz: i64) -> BV<'ctx> {
+    match e {
+        E::Add(o) => x + BV::from_i64(x.get_ctx(), 0, x.get_size()),
+        E::Sub(o) => x - BV::from_i64(x.get_ctx(), 0, x.get_size()),
+        E::Identity => *x,
+        E::Min => BV::from_i64(x.get_ctx(), 0, x.get_size()),
+        E::Max => BV::from_i64(x.get_ctx(), sz - 1, x.get_size()),
+    }
+}
+
 fn gen_coor_bounds<'ctx>(ex: &E, ey: &E, size: Size, x: &BV<'ctx>, y: &BV<'ctx>) -> Bool<'ctx> {
     Bool::and(x.get_ctx(), &[&gen_bounds_check(ex, x, size.x), &gen_bounds_check(ey, y, size.y)])
 }
 
+struct Effect<'ctx> {
+    x: &'ctx E,
+    y: &'ctx E,
+    pred: &'ctx Dynamic<'ctx>,
+    tp: i64,
+}
+
 struct SymbolicBoard<'ctx> {
     prefix: String,
+    size: Size,
     symbols: Vec<Vec<Dynamic<'ctx>>>,
 }
 
@@ -55,8 +73,40 @@ impl<'ctx> SymbolicBoard<'ctx> {
         let this = Self {
             prefix: String::new(),
             symbols,
+            size,
         };
         (this, Bool::and(&solver.ctx, &all.iter().collect::<Vec<_>>()))
+    }
+
+    fn rec_effect(&self, effects: &[Effect<'_>], x: &BV<'ctx>, y: &BV<'ctx>, tpe: &BV<'ctx>, symbol: &Dynamic<'ctx>, xid: usize, yid: usize) -> Bool<'ctx> {
+        match effects {
+            &[] => self.symbols[xid][yid]._eq(symbol),
+            &[hd, ..] => {
+                let tpe_ = tpe._eq(&BV::from_i64(tpe.get_ctx(), hd.tp, tpe.get_size()));
+                let x_ = e_to_bv(&hd.x, x, self.size.x)._eq(&BV::from_i64(x.get_ctx(), xid as _, x.get_size()));
+                let y_ = e_to_bv(&hd.y, y, self.size.x)._eq(&BV::from_i64(y.get_ctx(), yid as _, y.get_size()));
+                Bool::and(symbol.get_ctx(), &[&tpe_, &x_, &y_]).ite(&symbol._eq(&hd.pred), &self.rec_effect(&effects[1..], x, y, tpe, symbol, xid, yid))
+            },
+        }
+    }
+
+    fn effect(&self, effects: &[Effect<'_>], x: &BV<'ctx>, y: &BV<'ctx>, tpe: &BV<'ctx>, solver: &Solver<'ctx>) -> (Bool<'ctx>, Self) {
+        let prefix = format!("_{}", self.prefix);
+        let symbols: Vec<Vec<Dynamic<'ctx>>> = (0..self.size.x)
+            .map(|x| (0..self.size.y).map(|y| Datatype::new_const(&solver.ctx, format!("x{}y{}", x, y), &solver.pred_datatype.sort).into()).collect())
+            .collect();
+        let all = (0..symbols.len()).flat_map(|x| repeat(x).zip(0..symbols[0].len()))
+            .map(|(xid, yid)| {
+                self.rec_effect(effects, x, y, tpe, &symbols[xid][yid], xid, yid)
+            })
+            .collect::<Vec<_>>();
+        let b = Bool::and(x.get_ctx(), &all.iter().collect::<Vec<_>>());
+        let this = Self { 
+            prefix, 
+            size: self.size, 
+            symbols 
+        };
+        (b, this)
     }
 }
 
@@ -71,7 +121,7 @@ struct Solver<'ctx> {
     x_sz: u32,
     y_sz: u32,
     size: Size,
-    board: Vec<(BV<'ctx>, BV<'ctx>, Dynamic<'ctx>)>,
+    board: SymbolicBoard<'ctx>,
 }
 
 impl<'ctx> Solver<'ctx> {
@@ -170,20 +220,16 @@ impl<'ctx> Solver<'ctx> {
         Bool::or(self.ctx, &ors.iter().collect::<Vec<&Bool<'ctx>>>())
     }
 
-    fn effect_action(&mut self, effect: &Condition, x: i64, y: i64) {
-        for subcondition in &effect.sub_cond {
-            match subcondition {
-                SubCondition::Id { pred, x_e, y_e } => {
-                    let x = x_e.noramlize(x, self.size.x);
-                    let y = y_e.noramlize(y, self.size.y);
-                    let p = self.pred_to_z3(*pred).clone();
-                    let x = BV::from_i64(self.ctx, x, self.x_sz);
-                    let y = BV::from_i64(self.ctx, y, self.y_sz);
-                    self.board.push((x, y, p));
-                },
-                SubCondition::Not { .. } => panic!("Not cannot be used in effect"),
-            }
-        }
+    fn effect_action(&self, actions: &[Action], x: &BV<'ctx>, y: &BV<'ctx>, tpe: &BV<'ctx>, board: &SymbolicBoard<'ctx>) -> (Bool<'ctx>, SymbolicBoard<'ctx>) {
+        let effects: Vec<Effect> = actions.iter()
+            .enumerate().
+            flat_map(|(idx, action)| action.effect.sub_cond.iter()
+                .map(|cond| match cond {
+                    SubCondition::Id { pred, x_e, y_e } => Effect { x: x_e, y: y_e, pred: self.pred_to_z3(*pred), tp: idx as _ },
+                    _ => panic!("Cannot not as an effect"),
+                }))
+            .collect();
+        board.effect(&effects, x, y, tpe, self)
     }
 
     fn solve_black(&mut self, depth: u64) -> Bool<'ctx> {
